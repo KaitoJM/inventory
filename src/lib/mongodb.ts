a9c88bc561322229762855834ba8bcf55c8ff357
JM Mac <johnmarkmancol@gmail.com>
2025-01-02 12:39:55 +0800

    fix: builds

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -7,20 +7,17 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
-let clientPromise: Promise<MongoClient>;
-
-if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable to preserve the client.
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
-  }
-  clientPromise = global._mongoClientPromise;
-} else {
-  // In production mode, create a new client.
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+// if (process.env.NODE_ENV === "development") {
+//   // In development mode, use a global variable to preserve the client.
+//   if (!global._mongoClientPromise) {
+//     client = new MongoClient(uri, options);
+//     global._mongoClientPromise = client.connect();
+//   }
+//   clientPromise = global._mongoClientPromise;
+// } else {
+// In production mode, create a new client.
+const client = new MongoClient(uri, options);
+const clientPromise = client.connect();
+// }
 
 export default clientPromise;
